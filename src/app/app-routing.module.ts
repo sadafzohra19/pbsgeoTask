51d0c37c5426edb781290bfda169be29204a11b0
sadaf <sadafzohra19@gmail.com>
2021-01-16 16:32:48 +0500

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyMapComponent } from './my-map/my-map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mymap',
+    component: MyMapComponent,
+  },
+  {
+    path: '**',
+    component: MyMapComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
